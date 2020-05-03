d47cccd8a7103d044b5d1de44afdf763f9595d91
Josh <josh@somber.dev>
2020-05-02 22:56:44 -0600

    Added safe mode script for Pi

---
+++ cumboxSafeMode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Script for unfucking phone (booting safe mode) on rpi
+#disables cumbox temporarily ;)
+echo disabling cumbox script (temporarily)...
+cd /etc/udev/rules.d/
+sudo mv 01-cumbox.rules 01-cumbox.rules.bak
+echo reloading udev rules...
+sudo udevadm control --reload-rules && udevadm trigger
+echo starting checkra1n...
+sudo /opt/cumbox/checkra1n -c -s
+echo re-enabling cumbox and reloading udev rules...
+sudo mv 01-cumbox.rules.bak 01-cumbox.rules
+sudo udevadm control --reload-rules && udevadm trigger
