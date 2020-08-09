cb98f65dfaff40201dc3a7edfe2bd81a886ff5fa
Josh <josh@somber.dev>
2020-08-08 22:58:16 -0600

    Updates and shit

--- README.md
+++ README.md
@@ -12,10 +12,11 @@ cd cumbox
 
 Run The appropriate Installer (arm, arm64, i486):
 
-./cumscriptPi.sh
+./cumscriptPi_arm.sh
 ./cumscriptArm64.sh
-./cumscriptIntel.sh
+./cumscripti486.sh
+./cumscriptX86_64.sh
 
-Intalled checkra1n version is 0.10.1 beta
+Installed checkra1n version is 0.10.1 beta
 
 Just Plug In Your Device and Enter DFU. Profit.
--- cumscripti486.sh
+++ cumscripti486.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo mkdir -p /opt/cumbox
 sudo chmod 777 /opt/cumbox
-wget -O /opt/cumbox/checkra1n https://assets.checkra.in/downloads/linux/cli/i486/9b7a5c7821c8e06a334b854c5ffad7b28c56a5ac261afe3c6b647c9ba7185aee/checkra1n-i486
+wget -O /opt/cumbox/checkra1n https://assets.checkra.in/downloads/linux/cli/i486/53d45283b5616d9f0daa8a265362b65a33ce503b3088528cc2839544e166d4c6/checkra1n
 sudo chmod +x /opt/cumbox/checkra1n
 sudo cp 01-cumbox.rules /etc/udev/rules.d
 echo "cumbox is now installed!"
